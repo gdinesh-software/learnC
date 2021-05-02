1253144045ac89693097f744e97fc57fba70c449
Admin <criticalopsnoob1302@gmail.com>
2021-05-02 22:30:47 +0530

    something

--- chapter3/listing3-6.c
+++ chapter3/listing3-6.c
@@ -7,6 +7,6 @@ So i32_t is always 32 bits in any machine regardless of it's architecture!!! */
 int main(void) {
     int64_t aValue = 10000000000000;
     printf("aValue: %d\n", aValue);
-    printf("aValue = %" PRId64 "\n", aValue);
+    printf("aValue = %" PRId64 "\n", aValue); //PRId64 is a macro that is used for substitution in printf statements
     return 0;
 }
